b89cd75ea85543085cc6c494f3321bd7dbc83bbb
PsychoHacks <jamie.v732@gmail.com>
2017-11-10 10:00:51 +0000

    Initial commit

---
+++ S00162685Rad2017Mvc1/Models/UserRoles.sql
@@ -0,0 +1,4 @@
+﻿SELECT AspNetUsers.UserName, AspNetRoles.Name
+FROM AspNetUsers, AspNetRoles, AspNetUserRoles
+WHERE AspNetUsers.id = AspNetUserRoles.UserId
+AND AspNetRoles.id = AspNetUserRoles.RoleId
